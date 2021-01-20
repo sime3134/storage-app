dc6efd1be5a97878039370358cc45b077ae31970
sime3134 <simon_jern@msn.com>
2021-01-20 21:59:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  //NOTIFICATION DROPDOWN
+  document.querySelector('#noti_icon').addEventListener('click', () => {
+    if (document.getElementById("sideNav").offsetWidth != "0") {
+      document.getElementById("sideNav").style.width = "0";
+      document.body.style.backgroundColor = "rgba(92, 219, 149, 1)";
+      const collapseBars = document.querySelector('.collapseContainer');
+      collapseBars.classList.toggle('change');
+    }
+    const noti_dropdown = document.querySelector('.noti_dropdown');
+    noti_dropdown.classList.toggle('show');
+  });
+
+  //SIDENAV
+  document.querySelector('#toggleSideNav_button').addEventListener('click', () => {
+    if (document.getElementById("sideNav").offsetWidth == "0") {
+      const collapseBars = document.querySelector('.collapseContainer');
+      collapseBars.classList.toggle('change');
+      const noti_dropdown = document.querySelector('.noti_dropdown');
+      noti_dropdown.classList.remove('show');
+      document.getElementById("sideNav").style.width = "250px";
+      document.body.style.backgroundColor = "rgba(92, 219, 149, 0.4)";
+    } else if (document.getElementById("sideNav").offsetWidth == "250") {
+      const collapseBars = document.querySelector('.collapseContainer');
+      collapseBars.classList.toggle('change');
+      document.getElementById("sideNav").style.width = "0";
+      document.body.style.backgroundColor = "rgba(92, 219, 149, 1)";
+    }
+  });
+
+  //FORM LABEL - SELECT TAG
+  let occup = document.querySelector('#occup');
+  occup.addEventListener('change', () => {
+      document.querySelector('#occup-label').classList.replace("label-text", "label-text-up")
+  });
+
+  let gender = document.querySelector('#gender');
+  gender.addEventListener('change', () => {
+      document.querySelector('#gender-label').classList.replace("label-text", "label-text-up")
+  });
+});
